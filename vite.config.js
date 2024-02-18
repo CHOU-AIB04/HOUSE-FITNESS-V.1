cd18adc328801ae27a017edc48b20e0800266c5b
CHOU-AIB04 <142614891+CHOU-AIB04@users.noreply.github.com>
2024-02-18 16:31:54 +0100

    adding project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/HOUSE-FITNESS-V2/",
+  base : "/HOUSE-FITNESS-V.1/",
   plugins: [react()],
 })
